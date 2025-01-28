499c5b2791590ebd24403e371fd8de804496f916
ahmed <ahmed.elmohanned@gmail.com>
2025-01-29 01:02:08 +0200

    added proptype validation for the Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,37 @@
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
-function Navbar({currentPage}) {
+function Navbar({ currentPage }) {
   return (
     <div className={styles.nav}>
-      <h1>shopppyy</h1>
+      <h1>Shopppyy</h1>
       <nav>
-        {currentPage == "home" ? <Link className={styles.active + " " + styles.link} to="/">Home</Link> : <Link className={styles.link} to="/">Home</Link> }
-        {currentPage == "shop" ? <Link className={styles.active + " " + styles.link} to="/shop">Shop</Link> : <Link className={styles.link} to="/shop">Shop</Link> }
+        {currentPage == "home" ? (
+          <Link className={styles.active + " " + styles.link} to="/">
+            Home
+          </Link>
+        ) : (
+          <Link className={styles.link} to="/">
+            Home
+          </Link>
+        )}
+        {currentPage == "shop" ? (
+          <Link className={styles.active + " " + styles.link} to="/shop">
+            Shop
+          </Link>
+        ) : (
+          <Link className={styles.link} to="/shop">
+            Shop
+          </Link>
+        )}
       </nav>
     </div>
   );
 }
+
+Navbar.propTypes = {
+  currentPage: PropTypes.string.isRequired,
+};
 
 export default Navbar;
